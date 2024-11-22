23e0b7e2b25fd4d42d86fe40977af94bdc62c0d5
Ceevalonly <jason.jonathan545@gmail.com>
2024-11-22 15:35:01 +0700

    gas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,6 @@ dependencies {
     implementation(libs.firebase.database.ktx)
     implementation ("com.google.android.gms:play-services-maps:18.1.0")
     implementation ("com.google.android.gms:play-services-location:21.0.1")
-    implementation ("androidx.fragment:fragment-ktx:1.6.1")
-
 //    implementation(libs.mediation.test.suite)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
